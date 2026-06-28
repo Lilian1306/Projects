adf1848bd935c5730f4f1187fec422287007bd7e
Lilian1306 <kbts2207@gmail.com>
2026-06-28 17:20:34 -0600

    Squashed 'e-commerce/' content from commit 8fcd97e
    git-subtree-dir: e-commerce
    git-subtree-split: 8fcd97e2022726888a0ea7af3b5e57f181eba64d

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+import { Link } from 'react-router-dom'
+
+export default function Footer() {
+  return (
+    <div>
+    <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
+      <div>
+       <Link to='/'  onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}> <img src={assets.logo} alt='' className='mb-5 w-32'/> </Link> 
+        <p className='w-full md:w-2/3 text-gray-600'>
+        is simply dummy text of the printing and typesetting industry
+        </p>
+      </div>
+      <div>
+        <p className='text-xl font-medium mb-5'>Nuestra empresa</p>
+        <ul className='flex flex-col gap-1 text-gray-600'>
+         <Link to='/collection' onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}> <li>Colección</li> </Link> 
+           <Link to='/about' onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}> <li>Sobre Nosotros</li> </Link>
+        </ul>
+        
+      </div>
+      
+      <div>
+        <p className='text-xl font-medium mb-5'>Contáctanos</p>
+        <ul className='flex flex-col gap-1 text-gray-600'>
+          <li>+502 7777-7777</li>
+          <li>contact@website.com</li>
+        </ul>
+      </div>
+    </div>
+
+    <div>
+       <hr/>
+       <p className='py-5 text-sm text-center'>Copyright 2024@ website.com - All Right Reserved</p>
+    </div>
+   </div>
+  )
+}
---
+++ frontend/src/pages/Contact.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import Title from '../components/Title'
+import { assets } from '../assets/assets'
+
+export default function Context() {
+  const handleRedirect = (url) => {
+    window.open(url, '_blank')
+  }
+
+  return (
+    <div>
+      <div className='text-center text-2xl pt-10 border-t'>
+        <Title text1={'Nuestras'} text2={'Redes Sociales'}/>
+      </div>
+
+      <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-20'>
+       <img className='w-full md:max-w-[480px]' src={assets.contact_img} alt=''/>
+       <div className='flex flex-col justify-center items-start gap-6'>
+         <p className='font-semibold text-xl text-gray-600'>Nuestra Tienda</p>
+         <p className='text-gray-500'>WEB SITE BOUTIQUE ✨, <br/>  Entrada, Comalapa</p>
+         <p className="font-semibold text-xl text-gray-600">Nuestras Redes Sociales</p>
+       <div className="flex flex-row items-center gap-4">
+         <img 
+           src={assets.facebook} 
+           alt="Facebook" 
+           className="w-12 h-12 cursor-pointer"
+           onClick={() => handleRedirect('https://www.facebook.com')} />
+         <img 
+           src={assets.instagram} 
+           alt="Instagram" 
+           className="w-12 h-12 cursor-pointer"
+           onClick={() => handleRedirect('https://www.instagram.com')} />
+         <img 
+           src={assets.tiktok} 
+           alt="TikTok" 
+           className="w-12 h-12 cursor-pointer"
+           onClick={() => handleRedirect('https://www.tiktok.com')} />
+       </div>
+    </div>
+       </div>
+    </div>
+  )
+}
